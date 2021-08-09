b52379195ce9619ee7b0da38228e4e62d46fbd85
H Bui <86685688+csstudent01@users.noreply.github.com>
2021-08-09 18:59:25 +0000

    na

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 
-void welcome(){
+void welcomefnc(){
   cout << "Welcome to my Jelly Bean estimate program!!";
 }
 
 int calcJBVolume(double beanLength, double beanHeight){
   double beanVolume;
+  const double PI = 
 
   beanVolume = (5 * PI * beanLength * beanHeight * beanHeight) / 24;
 
@@ -23,22 +24,22 @@ int calcEstimate (double beanVolume, int jarVolume){
 }
 
 
-
-
 int main(){
 
 double avgBean = 0;
 int count = 0;
-double largestBean;
+double beanLength, beanHeight, jarVolume, largeBeanLength, largeBeanHeight, largeJarVolume, largestBean, numBean;
+
+cout << welcomefnc << endl;
 
 do{
   
-  cout << welcome << endl;
   cout << "Enter jelly bean length (cm) jelly bean height, and jar size separted by space: ";
   cin >> beanLength >> beanHeight >> jarVolume;
 
-  cout << " Estimate of jelly beans in the jar: " << numBean << endl;
-  avgBean = avgBean + numBean;
+  cout << " Estimate of jelly beans in the jar: " << calcEstimate(double beanVolume, int jarVolume) << endl;
+
+  avgBean = avgBean + calcEstimate(double beanVolume, int jarVolume);
 
   if(numBean > largestBean){
     largeBeanLength = beanLength;
@@ -49,7 +50,12 @@ do{
   count++;
 }while(( beanLength !=0) && (beanHeight !=0) && (jarVolume !=0));
 
+avgBean =avgBean / count;
 
-
+cout << "Total number entries: " << count << endl;
+cout << "Average number of beans: " << avgBean << endl;
+cout << "Bean size for the largest bean estimate: " << largeBeanLength <<" cm" << largeBeanHeight << " cm" << endl;
+cout << "Jar size for the largest jelly bean estimate: " << largeJarVolume << " cm" << endl;
+cout << "Thank you for using my program."
 return 0;
 }
