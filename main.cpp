741444647e47b0bc931be41d1c57a336aaacf74c
H Bui <86685688+csstudent01@users.noreply.github.com>
2021-08-09 06:47:11 +0000

    a04

--- main.cpp
+++ main.cpp
@@ -2,30 +2,54 @@
 using namespace std;
 
 void welcome(){
-  cout << "Welcome to my Jelly Bean estimate program!!"
+  cout << "Welcome to my Jelly Bean estimate program!!";
 }
 
 int calcJBVolume(double beanLength, double beanHeight){
   double beanVolume;
-  
-    beanVolume = (5 * PI * beanLength * beanHeight * beanHeight) / 24;
-  
+
+  beanVolume = (5 * PI * beanLength * beanHeight * beanHeight) / 24;
+
   return beanVolume;
+}
 
-int calcEstimate ()
+int calcEstimate (double beanVolume, int jarVolume){
+  const double loadFactor = 0.745;
+  double numBean;
 
+  numBean = (jarVolume * loadFactor) / beanVolume;
 
+  return numBean;
 }
 
 
 
 
+int main(){
+
+double avgBean = 0;
+int count = 0;
+double largestBean;
+
+do{
+  
+  cout << welcome << endl;
+  cout << "Enter jelly bean length (cm) jelly bean height, and jar size separted by space: ";
+  cin >> beanLength >> beanHeight >> jarVolume;
+
+  cout << " Estimate of jelly beans in the jar: " << numBean << endl;
+  avgBean = avgBean + numBean;
+
+  if(numBean > largestBean){
+    largeBeanLength = beanLength;
+    largeBeanHeight = beanHeight;
+    largeJarVolume = jarVolume;
+    largestBean = numBean;
+  }
+  count++;
+}while(( beanLength !=0) && (beanHeight !=0) && (jarVolume !=0));
 
 
-
-
-
-int main() {
 
 return 0;
 }
