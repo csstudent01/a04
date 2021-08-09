15a7b960306240cbfb95de41f05da7e690e242d4
VBKE15 <86685688+csstudent01@users.noreply.github.com>
2021-08-09 04:39:42 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void welcome(){
+  cout << "Welcome to my Jelly Bean estimate program!!"
+}
+
+int calcJBVolume(double beanLength, double beanHeight){
+  double beanVolume;
+  
+    beanVolume = (5 * PI * beanLength * beanHeight * beanHeight) / 24;
+  
+  return beanVolume;
+
+int calcEstimate ()
+
+
+}
+
+
+
+
+
+
+
+
+
+int main() {
+
+return 0;
+}
